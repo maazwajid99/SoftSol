5c47c21421729d25d03db6c596f00e60ddc73179
Muhammad Maaz <mmaaz.it@gmail.com>
2025-07-03 10:01:28 +0500

    Add Our Team In Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/hero-section";
 import Brand from "../components/Brand";
 import styled from "styled-components";
+import OurTeam from "../pages/OurTeam";
 import ReviewClient from "../components/ReviewsClient";
 import Footer from "../components/Footer";
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
     <Navbar />
     <HeroSection />
     <Brand />
+    <OurTeam/>
     <ReviewClient />
     <Footer/>
 
