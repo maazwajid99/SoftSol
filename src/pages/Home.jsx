197d32b381694b74e0853b660e3a0cf433083da3
Muhammad Maaz <mmaaz.it@gmail.com>
2025-07-05 03:04:34 +0500

    name change MarketplaceHero again  changing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSection from "../components/hero-section";
 import Brand from "../components/Brand";
 import styled from "styled-components";
 import TranscendPlatform from "../components/TranscendPlatform";
-import MarketplaceHero from "../components/marketplacehero";
+import MarketplaceHero from "../components/MarketplaceHero";
 import Comprehensive from "../components/Comprehensive";
 import TeamSection from "../components/TeamSection";
 import ReviewClient from "../components/ReviewsClient";
